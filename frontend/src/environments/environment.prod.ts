28792287925396cc10ba7c577aa091abad4ffc8d
jhechus <jess.osm1412@gmail.com>
2026-06-28 17:37:47 -0600

    fix: apiUrl produccion apuntando a backend render

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://la-terraza-api.onrender.com', // reemplaza con tu URL real de Render
+  apiUrl: 'https://la-terraza-rwuz.onrender.com',
 };
